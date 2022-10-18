89ae5ef38482eedb1e797063e82309ec04e66ee0
Krish Shah <shahkrish2016@gmail.com>
2022-10-17 20:34:20 -0400

    added week 1 content

---
+++ POTW/week-1/Cssolution1.java
@@ -0,0 +1,25 @@
+/*
+ * Avinash Anand
+ * October 17, 2022
+ * This program will print out the times table from 1 to 12, and the if statements format the table.
+ */
+package cssolution1;
+
+public class Cssolution1 {
+    public static void main(String[] args) {
+        int num = 0;
+        for(int i = 1; i <= 12; i++){
+            for(int j = 1; j <= 12; j++){
+                num = i * j;
+                if(num < 10){
+                    System.out.print(num + "   ");
+                } else if(num >= 10 && num < 100){
+                    System.out.print(num + "  ");
+                } else if(num >= 100){
+                    System.out.print(num + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
