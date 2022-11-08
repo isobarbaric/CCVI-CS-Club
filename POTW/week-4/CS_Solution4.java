a8aee489b605686794aac0951022129d37fb6cc0
Krish Shah <76544615+isobarbaric@users.noreply.github.com>
2022-11-08 17:43:48 -0500

    formatting update

--- POTW/week-4/CS_Solution4.java
+++ POTW/week-4/CS_Solution4.java
@@ -10,10 +10,10 @@ public class CS_Solution4 {
 
     public static void main(String[] args) {
         // Ask the user for the values of n and k
-        int n = Integer.parseInt(JOptionPane.showInputDialog ("what is the value of n"));
-        int k = Integer.parseInt(JOptionPane.showInputDialog ("what is the value of k"));
+        int n = Integer.parseInt(JOptionPane.showInputDialog ("What is the value of n?"));
+        int k = Integer.parseInt(JOptionPane.showInputDialog ("What is the value of k?"));
     
-        // use a for loop to multiplu n by every number up untill k
+        // use a for loop to multiply n by every number up until k
         for (int i = 1; i <= k; i++) {
             System.out.println(n+ "+" + i + "=" +n*i);
         }
