6c69133be3401ac31dc7392acc3d6453b2dd4645
Krish Shah <76544615+isobarbaric@users.noreply.github.com>
2022-11-08 17:43:07 -0500

    standardized nomenclature

--- POTW/week-4/CS_Solution4.java
+++ POTW/week-4/CS_Solution4.java
@@ -2,15 +2,17 @@
 Jason Yu
 Nov, 7, 2022
  */
-package potw.pkg4;
+package potw;
 
 import javax.swing.JOptionPane;
 
-public class POTW4 {
+public class CS_Solution4 {
+
     public static void main(String[] args) {
         // Ask the user for the values of n and k
-        int n= Integer.parseInt(JOptionPane.showInputDialog ("what is the value of n"));
+        int n = Integer.parseInt(JOptionPane.showInputDialog ("what is the value of n"));
         int k = Integer.parseInt(JOptionPane.showInputDialog ("what is the value of k"));
+    
         // use a for loop to multiplu n by every number up untill k
         for (int i = 1; i <= k; i++) {
             System.out.println(n+ "+" + i + "=" +n*i);
