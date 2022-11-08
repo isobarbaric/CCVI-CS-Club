68feeb127946ddd0fa0c42e40452fff66211a87d
Krish Shah <shahkrish2016@gmail.com>
2022-11-08 18:01:09 -0500

    updated RNG script

--- POTW/winner.py
+++ POTW/winner.py
@@ -1,5 +1,16 @@
 import random
 
-winner = random.randint(1, int(input('Enter the number of contenders: ')))
+n = int(input('Enter the number of contenders: '))
+
+winner = -1
+seen = []
+
+while True:
+    num = random.randint(1, n)
+    if num in seen:
+        winner = num
+        break
+    else:
+        seen.append(num)
 
 print(winner)
