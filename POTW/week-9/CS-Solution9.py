e862b73094699df7111bc3e19c85beb4aff6d514
AAnand05 <44912634+fligmiger@users.noreply.github.com>
2022-12-11 23:03:54 -0500

    Add files via upload

---
+++ POTW/week-9/CS-Solution9.py
@@ -0,0 +1,11 @@
+n = int(input())
+if n%5 == 1:
+    print("Ham and Cheese")
+elif n%5 == 2:
+    print("PB and Jelly")
+elif n%5 == 3:
+    print("BLT")
+elif n%5 == 4:
+    print("Tuna")
+elif n%5 == 0:
+    print("Grilled Cheese")
